02e70dc8ccc35ad4f4553a863c4664b6a3dcd027
Kushanyasiru97 <kushanyasiruwalpola@gmail.com>
2024-10-04 04:04:16 +0530

    Storing Data part Completed

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,46 +1,6 @@
-
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
-import PostForm from "@/components/post-form";
+import { createPost } from '@/actions/posts';
+import PostForm from '@/components/post-form';
 
 export default function NewPostPage() {
-
-  async function createPost(formData) {
-    "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let error = [];
-
-    if(!title || title.trim().length===0){
-      error.push("Titile is Required")
-    }
-
-    if(!content || content.trim().length===0){
-      error.push("Content is Required")
-    }
-
-    if(!image){
-      error.push("Image is Required")
-    }
-
-    if(error.length > 0){
-      return {error};
-    }
-
-    storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    });
-
-    redirect("/feed");
-  }
-
-  return (
-    <PostForm action={createPost} />
-  )
-
+  return <PostForm action={createPost} />;
 }
