ae1c6c7525f511d702432d7a1b8ee668c3d11335
Kushanyasiru97 <kushanyasiruwalpola@gmail.com>
2024-10-05 05:42:06 +0530

    Dynamic Metadata Added..

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,20 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+/*export const metadata ={
+  title: "Browse all our X posts",
+  description: "Browse all our Posts"
+}*/
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return{
+    title:`Brows all our ${numberOfPosts} posts`,
+    description:"Brows All our Posts"
+  }
+
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
